dd03778026452378b308e4ce5c4afa60cdca3d20
gravitano <gue.warsono@gmail.com>
2014-08-19 07:47:57 +0700

    Fix sessions layout and updated default title in admin master page

--- app/views/admin/layouts/master.blade.php
+++ app/views/admin/layouts/master.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" class="app">
 <head>  
   <meta charset="utf-8" />
-  <title>@yield('title', 'Hello!')</title>
+  <title>@yield('title', 'Administrator')</title>
   <meta name="description" content="" />
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> 
   <link rel="stylesheet" href="{{ a('css/bootstrap.css') }}" type="text/css" />
--- app/views/layouts/sessions.blade.php
+++ app/views/layouts/sessions.blade.php
@@ -32,12 +32,12 @@
     </div>
   </footer>
   <!-- / footer -->
-  <script src="js/jquery.min.js"></script>
+    <script src="{{ a('js/jquery.min.js') }}"></script>
   <!-- Bootstrap -->
-  <script src="js/bootstrap.js"></script>
+  <script src="{{ a('js/bootstrap.js') }}"></script>
   <!-- App -->
-  <script src="js/app.js"></script>  
-  <script src="js/slimscroll/jquery.slimscroll.min.js"></script>
-    <script src="js/app.plugin.js"></script>
+  <script src="{{ a('js/app.js') }}"></script>  
+  <script src="{{ a('js/slimscroll/jquery.slimscroll.min.js') }}"></script>
+    <script src="{{ a('js/app.plugin.js') }}"></script>
 </body>
 </html>
